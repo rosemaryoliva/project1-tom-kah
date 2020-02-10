223b65f0d2dfed386616e3bc211d65f62d2987dc
Rosemary Oliva <roliva@pdx.edu>
2020-02-09 19:40:24 -0800

    Added images

--- javascript.js
+++ javascript.js
@@ -15,3 +15,50 @@
           }
         });
     });
+
+// Mushroom Chopping 
+
+    $('.food-item').click(function(){
+      $('#food-item1').slideUp(300).fadeOut('slow')
+      $('food-item').remove('#food-item1').delay(1000)
+      //$('#food-item2').fadeIn(400) 
+    })
+
+     $('#food-item2').click(function(){
+       $('#food-item2').slideUp(300)
+      //$('#food-item2').remove('#food-item2')
+       $('food-item').remove('#food-item2').delay(1000)
+       $('#food-item3').fadeIn(400)
+       //$('#food-item3').toggleClass('hide')
+
+     })
+     $('#food-item3').click(function(){
+       $('#food-item3').slideUp(300)
+      //$('#food-item2').remove('#food-item2')
+       $('food-item').remove('#food-item3').delay(1000)
+       $('#food-item4').fadeIn(400)
+       //$('#food-item3').toggleClass('hide')
+     })
+
+
+     $('.onion-item').click(function(){
+      $('#onion-item1').slideUp(300).fadeOut('slow')
+      $('onion-item').remove('#onion-item1').delay(1000)
+      //$('#food-item2').fadeIn(400) 
+    })
+
+     $('#onion-item2').click(function(){
+       $('#onion-item2').slideUp(300)
+      //$('#food-item2').remove('#food-item2')
+       $('onion-item').remove('#onion-item2').delay(1000)
+       $('#onion-item3').fadeIn(400)
+       //$('#food-item3').toggleClass('hide')
+
+     })
+     $('#onion-item3').click(function(){
+       $('#onion-item3').slideUp(300)
+      //$('#food-item2').remove('#food-item2')
+       $('onion-item').remove('#onion-item3').delay(1000)
+       $('#onion-item4').fadeIn(400)
+       //$('#food-item3').toggleClass('hide')
+     })
